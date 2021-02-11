2cc7b3ac2f0b0bffbc08e6527a99f0c48b50ac9d
RicoRos85 <rico.rosenkrans@inverto.dk>
2021-02-11 12:04:33 +0100

    Added last Classes to Models

---
+++ models/images.js
@@ -0,0 +1,7 @@
+class Image {
+    constructor(id, mimeType, size) {
+        this.id = id;
+        this.mimeType = mimeType;
+        this.size = size;
+    }
+}
---
+++ models/matches.js
@@ -0,0 +1,12 @@
+class Match {
+    constructor(firstUserId, firstUserLike, secondUserId, secondUserLike) {
+        this.firstUserId = firstUserId;
+        this.firstUserLike = firstUserLike;
+        this.secondUserId = secondUserId;
+        this.secondUserLike = secondUserLike;
+    }
+
+    isMatch(firstUserId, secondUserId) {
+
+    }
+}
